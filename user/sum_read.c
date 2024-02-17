660a34db443c27553320fb3328082c99d77a0642
roscherk <70962699+roscherk@users.noreply.github.com>
2024-02-17 17:16:20 +0300

    exc+debug: handle buffer overflow, add debug logging

--- user/sum_read.c
+++ user/sum_read.c
@@ -3,25 +3,34 @@
 #include "user/user.h"
 
 int read_number(int* result) {
-    const unsigned int BUFFER_SIZE = 33;  // по 16 байт на числа + один байт на \0
+    printf("DEBUG: entering read_number...\n");
+    const unsigned int BUFFER_SIZE = 33;  // по 16 байт на числа + один байт на \0 или \n
     char buffer[BUFFER_SIZE];
-    int read_success = 0, i;
-    for (i = 0; i + 1 < BUFFER_SIZE; ++i) {
+    int status = -2, i;
+    for (i = 0; i < BUFFER_SIZE; ++i) {
+        printf("\tDEBUG: i = %d\n", i);
         if (read(0, buffer + i, 1) <= 0) {
-            read_success = -1;
+            printf("\tDEBUG: io error\n");
+            status = -1;
             break;
         }
         if (buffer[i] == '\n' || buffer[i] == ' ') {
+            printf("\tDEBUG: all good\n");
+            status = 0;
             break;
         }
+        if (i == BUFFER_SIZE - 1 && buffer[i] != '\n' && buffer[i] != ' ') {
+            printf("\tDEBUG: buffer overflow\n");
+            status = -3;
+        }
+        printf("\tDEBUG: buffer[%d] = %c\n", i, buffer[i]);
     }
-    if (i == 0)  // пустая строка
-        return -2;
+    if (status)
+        return status;
     buffer[i] = '\0';
-    if (read_success == -1)
-        return -1;
     *result = atoi(buffer);
-    return 0;
+    printf("\tDEBUG: result = %d\n", *result);
+    return status;
 }
 
 void check_status(int status) {
@@ -32,18 +41,23 @@ void check_status(int status) {
         case -2:
             fprintf(2, "Error: format error. Enter two integers, separated by space.\n");
             exit(1);
+        case -3:
+            fprintf(2, "Error: buffer overflow, integer too long\n");
+            exit(1);
         default:
             return;
     }
 }
 
-int main(int argc, char* argv[]) {
+int main(int _, char* __[]) {
     int number1, number2, status;
 
     status = read_number(&number1);
     check_status(status);
     status = read_number(&number2);
     check_status(status);
-    printf("%d + %d = %d\n", number1, number2, number1 + number2);
+
+    printf("DEBUG: |%d %d|\n", number1, number2);
+    printf("%d\n", number1 + number2);
     exit(0);
 }
