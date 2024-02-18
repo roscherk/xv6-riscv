67e9467d226c2101d99fb259a46ecc8e66320655
roscherk <70962699+roscherk@users.noreply.github.com>
2024-02-18 11:41:45 +0300

    debug: remove excessive debug prints

--- user/sum_read.c
+++ user/sum_read.c
@@ -3,38 +3,30 @@
 #include "user/user.h"
 
 int read_number(int* result, char ending) {
-    printf("DEBUG: entering read_number...\n");
     const unsigned int BUFFER_SIZE = 33;  // по 16 байт на числа + один байт на \0 или \n
     char buffer[BUFFER_SIZE];
     int status = -2, i;
     for (i = 0; i < BUFFER_SIZE; ++i) {
-        printf("\tDEBUG: i = %d\n", i);
         if (read(0, buffer + i, 1) <= 0) {
-            printf("\tDEBUG: io error\n");
             status = -1;
             break;
         }
         if (buffer[i] == '\n' || buffer[i] == ' ') {
             if (buffer[i] == ending) {
-                printf("\tDEBUG: all good\n");
                 status = 0;
             } else {
-                printf("\tDEBUG: wrong format\n");
                 status = -2;
             }
             break;
         }
         if (i == BUFFER_SIZE - 1 && buffer[i] != ending) {
-            printf("\tDEBUG: buffer overflow\n");
             status = -3;
         }
-        printf("\tDEBUG: buffer[%d] = %c\n", i, buffer[i]);
     }
     if (status)
         return status;
     buffer[i] = '\0';
     *result = atoi(buffer);
-    printf("\tDEBUG: result = %d\n", *result);
     return status;
 }
 
