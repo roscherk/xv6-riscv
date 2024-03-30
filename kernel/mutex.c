9514abea18616f0e059130c7753bdc234edfd7d4
roscherk <70962699+roscherk@users.noreply.github.com>
2024-03-30 19:58:20 +0300

    task: done

--- kernel/mutex.c
+++ kernel/mutex.c
@@ -30,11 +30,8 @@ int mutex_create(void) {
       mutex->dcount++;
 
       struct proc *process = myproc();
-      printf("looking for a place...\n");
       for (int j = 0; j < NOMUTEX; ++j) {
-        printf("p->omutex[%d] = %d\n", j, process->omutex[j]);
         if (process->omutex[j] == 0) {
-          printf("found place %d, setting it to %d\n", j, &mtable[i]);
           process->omutex[j] = &mtable[i];
           md = j;
           break;
@@ -42,36 +39,25 @@ int mutex_create(void) {
       }
     }
     releasesleep(&mutex->lock);
-    printf("mutex->lock.locked = %d", mutex->lock.locked);
-    if (md != -1) {
-      printf("process->omutex[j]->lock.locked = %d", myproc()->omutex[md]->lock.locked);
+    if (md != -1)
       break;
-    }
   }
   release(&mtable_lock);
   return md;
 }
 
 int check_md(int md) {
-  printf("checking %d...\n", md);
   return md < 0 || md >= NOMUTEX ? -1 : md;
 }
 
 int mutex_lock(int md) {
-  printf("mutex lock %d\n", md);
-  for (int i = 0; i < NOMUTEX; ++i) {
-    printf("myproc()->omutex[%d] = %d\n", i, myproc()->omutex[i]);
-  }
   if (check_md(md) < 0)
     return -1;
-  printf("myproc()->omutex[md]->lock.locked = %d", myproc()->omutex[md]->lock.locked);
   acquiresleep(&myproc()->omutex[md]->lock);
-  printf("I AM ALIVE!!!");
   return 0;
 }
 
 int mutex_unlock(int md) {
-  printf("mutex unlock %d\n", md);
   if (check_md(md) < 0)
     return -1;
   releasesleep(&myproc()->omutex[md]->lock);
@@ -79,7 +65,6 @@ int mutex_unlock(int md) {
 }
 
 int mutex_release(int md) {
-  printf("mutex release %d\n", md);
   if (check_md(md) < 0)
     return -1;
   mutex_t *mutex = myproc()->omutex[md];
@@ -90,7 +75,6 @@ int mutex_release(int md) {
 
 mutex_t *mutex_dup(mutex_t *mutex) {
   mutex->dcount++;
-  printf("mutex.dcount = %d now!\n", mutex->dcount);
   return mutex;
 }
 
