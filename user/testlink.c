f337fe778d4af6b192bed634dac50a938172efff
roscherk <70962699+roscherk@users.noreply.github.com>
2024-05-20 18:23:44 +0300

    rewrite a little

--- user/testlink.c
+++ user/testlink.c
@@ -10,12 +10,12 @@
 char name_buf[MAXPATH], data_buf[DATA_SIZE];
 const char data[DATA_SIZE] = "Oh hi, Mark!\0";
 
-int check(int result, int expected, const char* message) {
-  if (result < expected) {
+void check(int result, int expected, const char *message) {
+  if ((expected >= 0 && result < expected) ||
+      (expected < 0 && result > expected)) {
     fprintf(2, "%s! Expected %d, got %d.\n", message, expected, result);
-    return -1;
+    exit(1);
   }
-  return 1;
 }
 
 void makefile(char *filename) {
@@ -25,152 +25,108 @@ void makefile(char *filename) {
   close(fd);
 }
 
-void read_data(const char *filename, char *buf) {
+void read_data(const char *filename, int fail) {
   int fd = open(filename, O_RDONLY);
-  check(fd, 0, "Could not open file");
-  check(read(fd, buf, DATA_SIZE), DATA_SIZE, "Could not read");
-  buf[DATA_SIZE - 1] = 0;
-//  printf("read_data: buf = `%s`\n", buf);
+  check(fd, -fail, "Could not open file");
+  check(read(fd, data_buf, DATA_SIZE), -fail, "Could not read");
+  data_buf[DATA_SIZE - 1] = 0;
+  //  printf("read_data: buf = `%s`\n", data_buf);
 }
 
-void arrange(const char* dir) {
+void arrange(const char *dir) {
   memset(name_buf, 0, MAXPATH);
   memset(data_buf, 0, DATA_SIZE);
   chdir(dir);
 }
 
-int test1() {
-  int pass = -1;
+void test1() {
   arrange("/");
 
   symlink("/f0", "l0_abs");
-  pass += check(readlink("l0_abs", name_buf), -1, "Readlink error");
+  check(readlink("l0_abs", name_buf), 0, "Readlink error");
 
-  read_data("l0_abs", data_buf);
-  pass += check(strcmp(data_buf, data), 0, "Contents do not match");
-
-  return pass;
+  read_data("l0_abs", 0);
+  check(strcmp(data_buf, data), 0, "Contents do not match");
 }
 
-int test2() {
-  int pass = -1;
+void test2() {
   arrange("/");
 
   symlink("./f0", "l0_rel");
-  pass += check(readlink("l0_rel", name_buf), 0, "Readlink error");
+  check(readlink("l0_rel", name_buf), 0, "Readlink error");
 
-  read_data("l0_rel", data_buf);
-  pass += check(strcmp(data_buf, data), 0, "Contents do not match");
-
-  return pass;
+  read_data("l0_rel", 0);
+  check(strcmp(data_buf, data), 0, "Contents do not match");
 }
 
-int test3() {
-  int pass = -1;
+void test3() {
   arrange("/d1/d2/d3");
 
   symlink("../../../f0", "l0_rel_up");
-  pass += check(readlink("l0_rel_up", name_buf), -1, "Readlink error");
-
-  read_data("l0_rel_up", data_buf);
-  pass += check(strcmp(data_buf, data), 0, "Contents do not match");
-
-  return pass;
+  read_data("l0_rel_up", 0);
+  check(strcmp(data_buf, data), 0, "Contents do not match");
 }
 
-int test4() {
-  int pass = -1;
+void test4() {
   arrange("/");
 
   symlink("./d1/d2/d3/f3", "l3_rel_down");
-  pass += check(readlink("l3_rel_down", name_buf), -1, "Readlink error");
-
-  read_data("l3_rel_down", data_buf);
-  pass += check(strcmp(data_buf, data), 0, "Contents do not match");
-
-  return pass;
+  read_data("l3_rel_down", 0);
+  check(strcmp(data_buf, data), 0, "Contents do not match");
 }
 
-int test5() {
-  int pass = -1;
+void test5() {
   arrange("/d1");
 
   symlink("/l0_abs", "l0_abs2abs");
-  pass += check(readlink("l0_abs2abs", name_buf), -1, "Readlink error");
-
-  read_data("l0_abs2abs", data_buf);
-  pass += check(strcmp(data_buf, data), 0, "Contents do not match");
-
-  return pass;
+  read_data("l0_abs2abs", 0);
+  check(strcmp(data_buf, data), 0, "Contents do not match");
 }
 
-int test6() {
-  int pass = -1;
+void test6() {
   arrange("/");
 
   symlink("/l0_rel", "l0_abs2rel");
-  pass += check(readlink("l0_abs2rel", name_buf), -1, "Readlink error");
-
-  read_data("l0_abs2rel", data_buf);
-  pass += check(strcmp(data_buf, data), 0, "Contents do not match");
-
-  return pass;
+  read_data("l0_abs2rel", 0);
+  check(strcmp(data_buf, data), 0, "Contents do not match");
 }
 
-int test7() {
-  int pass = -1;
+void test7() {
   arrange("/d1");
 
   symlink("../l0_rel", "l0_rel2rel");
-  pass += check(readlink("l0_rel2rel", name_buf), -1, "Readlink error");
-
-  read_data("l0_rel2rel", data_buf);
-  pass += check(strcmp(data_buf, data), 0, "Contents do not match");
-
-  return pass;
+  read_data("l0_rel2rel", 0);
+  check(strcmp(data_buf, data), 0, "Contents do not match");
 }
 
-int test8() {
-  int pass = -1;
-  return pass;
+void test8() {
+  arrange("/");
+
+  symlink("l_inf_rec", "l_inf_rec");
+  return read_data("lrec", 1);
 }
 
-int test9() {
-  int pass = -1;
-  return pass;
+void test9() {
+  arrange("/");
+
+  symlink("lrec0", "lrec1");
+  symlink("lrec1", "lrec2");
+  symlink("lrec2", "lrec0");
+  return read_data("lrec0", 1);
 }
 
-int test10() {
-  int pass = -1;
-  return pass;
-}
+void test10() {}
 
-int test11() {
-  int pass = -1;
-  return pass;
-}
+void test11() {}
 
-int test12() {
-  int pass = -1;
-  return pass;
-}
+void test12() {}
 
-static int (*tests[TESTS_COUNT])(void) = {
-    test1,
-    test2,
-    test3,
-    test4,
-    test5,
-    test6,
-    test7,
-    test8,
-    test9,
-    test10,
-    test11,
-    test12,
+static void (*tests[TESTS_COUNT])(void) = {
+    test1, test2, test3, test4,  test5,  test6,
+    test7, test8, test9, test10, test11, test12,
 };
 
-static char* test_names[TESTS_COUNT] = {
+static char *test_names[TESTS_COUNT] = {
     "correct absolute",
     "correct relative",
     "correct relative up",
@@ -185,26 +141,10 @@ static char* test_names[TESTS_COUNT] = {
     "relative to wrong path down",
 };
 
-//static char* link_names[TESTS_COUNT] = {
-//    "l0_abs",
-//    "l0_rel",
-//    "l0_rel_up",
-//    "l3_rel_down",
-//    "l0_abs2abs",
-//    "l0_abs2rel",
-//    "l0_rel2rel",
-//    "l_loop",
-//    "l_loop_rel",
-//    "l_abs2none",
-//    "l3_rel2none_up",
-//    "l0_rel2none_down",
-//};
-
 void run_test(uint index) {
   printf("--- Running test %d `%s`... ", index + 1, test_names[index]);
-  if (check(tests[index](), 0, "Fail") >= 0) {
-    printf("Success!\n");
-  }
+  tests[index]();
+  printf("Success!\n");
 }
 
 void make_files() {
